1f456c3be4b17229cf4385ed5217f0a9c6b329ef
gauravRandheer <gaurav.shrivastava.15csc@bml.edu.in>
2025-06-03 15:52:12 +0530

    Add new function for generating mock data

---
+++ utils/generateUrldata.js
@@ -0,0 +1,42 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { nanoid } = require("nanoid");
+
+
+
+const batchSize = 1000
+const TotalRecords = 1000000
+
+function generateMockData(batchSize) {
+  const data = [];
+
+  for (let i = 0; i < 10000; i++) {
+    data.push({
+      short_code: nanoid(6),
+      original_url: `https://example.com/${i}`,
+      user_id: 1,
+      expired_at: null,
+      password: null,
+    });
+  }
+
+  return data;
+}
+
+async function generateUrldata() {
+
+  for (let i = 0; i < TotalRecords; i+=batchSize) {
+    const batch = generateMockData(batchSize)
+      await prisma.url_shortener.createMany({
+        data:batch,
+        skipDuplicates:true
+      })
+   console.log(`Inserted batch ${i / batchSize + 1}`);
+  }
+
+ await prisma.$disconnect();;
+  console.log("Generate data completed");
+  process.exit(0);
+}
+
+generateUrldata();
